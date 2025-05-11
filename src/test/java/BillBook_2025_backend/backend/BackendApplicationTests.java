9b26ec0d503442d77fc45057565dcd0b5130a75f
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-05-12 01:44:13 +0900

    initial commit

---
+++ src/test/java/BillBook_2025_backend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package BillBook_2025_backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
