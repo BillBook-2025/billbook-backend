60d1554371382e88cb6b85bde0938b8abbe29146
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-08-05 19:03:01 +0900

    feat: 개인정보 조회 및 수정 기능 구현

--- src/main/java/BillBook_2025_backend/backend/service/UserService.java
+++ src/main/java/BillBook_2025_backend/backend/service/UserService.java
@@ -1,43 +1,45 @@
 package BillBook_2025_backend.backend.service;
 
-import BillBook_2025_backend.backend.entity.User;
-import BillBook_2025_backend.backend.repository.UserRepository;
+import BillBook_2025_backend.backend.dto.UserInfoDto;
+import BillBook_2025_backend.backend.entity.Member;
+import BillBook_2025_backend.backend.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    public User signup(User user) {
-        if (userRepository.findByUserId(user.getUserId()).isPresent()) {
+    public Member signup(Member member) {
+        if (memberRepository.findByUserId(member.getUserId()).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
 
-        User newUser = new User(user.getUserId(), user.getPassword(), user.getEmail(), user.getUserName());
-        return userRepository.save(newUser);
+        Member newMember = new Member(member.getUserId(), member.getPassword(), member.getEmail(), member.getUserName());
+        return memberRepository.save(newMember);
     }
 
-    public void delete(User user) {
-        if (!userRepository.findByUserId(user.getUserId()).isPresent()) {
+    public void delete(Member member) {
+        if (!memberRepository.findByUserId(member.getUserId()).isPresent()) {
             throw new IllegalArgumentException("해당 유저가 존재하지 않습니다.");
         }
 
-        userRepository.delete(user);
+        memberRepository.delete(member);
     }
 
-    public User login(User user) {
-        if(!userRepository.findByUserId(user.getUserId()).isPresent()) {
+    public Member login(Member member) {
+        if(!memberRepository.findByUserId(member.getUserId()).isPresent()) {
             throw new IllegalArgumentException("해당 아이디를 찾을 수 없습니다.");
         } else {
-            User finduser = userRepository.findByUserId(user.getUserId()).get();
-            if (!finduser.getPassword().equals(user.getPassword())) {
+            Member finduser = memberRepository.findByUserId(member.getUserId()).get();
+            if (!finduser.getPassword().equals(member.getPassword())) {
                 throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
             }
 
@@ -46,13 +48,42 @@ public class UserService {
     }
 
     @Transactional
-    public void changePassword(String userId, String password, String confirmPassword) {
+    public void changePassword(Long userId, String password, String confirmPassword) {
         if (!password.equals(confirmPassword)) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         } else {
-            User user = userRepository.findByUserId(userId).get();
-            user.setPassword(password);
+            Member member = memberRepository.findById(userId).get();
+            member.setPassword(password);
         }
+
+    }
+
+    public UserInfoDto getMyInfoDetails(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+        UserInfoDto userInfoDto = new UserInfoDto(member);
+        return userInfoDto;
+
+    }
+
+    @Transactional
+    public void updateInfo(Long id, UserInfoDto request) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+        if (request.getEmail() != null) {
+            member.setEmail(request.getEmail());
+        }
+        if (request.getPassword() != null) {
+            member.setPassword(request.getPassword());
+        }
+        if (request.getUserName() != null) {
+            member.setUserName(request.getUserName());
+        }
+        if (request.getTemperature() != 0){  // 온도가 0이게 되면
+            throw new IllegalArgumentException("온도는 임의로 변경할 수 없습니다.");
+        }
+        if (request.getUserId() != null) {
+            throw new IllegalArgumentException("아이디는 변경할 수 없습니다.");
+        }
+
 
     }
 }
