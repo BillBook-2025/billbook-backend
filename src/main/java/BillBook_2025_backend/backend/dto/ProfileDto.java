6e9a932ff9e3dc9483027e80e7b62dc091e80555
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-08-18 20:17:28 +0900

    feat: 프로필 관련 기능 구현

---
+++ src/main/java/BillBook_2025_backend/backend/dto/ProfileDto.java
@@ -0,0 +1,23 @@
+package BillBook_2025_backend.backend.dto;
+
+import BillBook_2025_backend.backend.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileDto {
+    private String username;
+    private Double temperature;
+    private String ProfilePic;
+    private Long buyNum;
+    private Long sellNum;
+
+    public ProfileDto(Member member, Long buyNum, Long sellNum) {
+        this.username = member.getUserName();
+        this.temperature = member.getTemperature();
+        this.ProfilePic = member.getImage().getUrl();
+        this.buyNum = buyNum;
+        this.sellNum = sellNum;
+    }
+}
--- src/main/java/BillBook_2025_backend/backend/repository/FollowRepository.java
+++ src/main/java/BillBook_2025_backend/backend/repository/FollowRepository.java
@@ -1,7 +1,12 @@
 package BillBook_2025_backend.backend.repository;
 
 import BillBook_2025_backend.backend.entity.Follow;
+import BillBook_2025_backend.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.lang.ScopedValue;
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Optional<Follow> findByFollowerAndFollowing(Member follower, Member following);
 }
